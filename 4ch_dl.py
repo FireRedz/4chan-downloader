18199c14c6e4529caf613348684bd2da7ded444e
Alif <44401509+FireRedz@users.noreply.github.com>
2020-02-02 13:15:41 +0800

    Update 4ch_dl.py
    remove unnecessary thing

--- 4ch_dl.py
+++ 4ch_dl.py
@@ -1,5 +1,4 @@
 import requests
-import wget
 import os
 
 r = requests.session()
@@ -12,7 +11,6 @@ def getImages(directory):
 	threadBoard = threadLink[3]
 	threadID = threadLink[5]
 
-	#link = 'https://a.4cdn.org/'+threadBoard+'/thread/'+threadID+'.json'
 
 	requestData = r.get('https://a.4cdn.org/'+threadBoard+'/thread/'+threadID+'.json')
 	threadData = requestData.json()['posts']
@@ -31,9 +29,7 @@ def getImages(directory):
 
 		if 'tim' in threadData[x]:
 			print('Downloading', threadData[x]['no'],str(threadData[x]['ext']) ,'| Size is ', str(int(threadData[x]['fsize']/1000)),'kb')
-			#wget.download('https://i.4cdn.org/'+str(threadBoard)+'/'+str(threadData[x]['tim'])+ str(threadData[x]['ext']), directory+'/'+str(threadBoard)+'/'+str(threadID)+'/'+str(threadData[x]['tim'])+str(threadData[x]['ext']))
 			os.system("aria2c "+'https://i.4cdn.org/'+str(threadBoard)+'/'+str(threadData[x]['tim'])+ str(threadData[x]['ext']) +" -d "+directory+'\\'+str(threadBoard)+'\\'+str(threadID)+ ' -x 10')
-			#print("aria2c "+'https://i.4cdn.org/'+str(threadBoard)+'/'+str(threadData[x]['tim'])+ str(threadData[x]['ext']) +" -d "+directory+'\\'+str(threadBoard)+'\\'+str(threadID))
 		else:
 			print('Skipping')
 
